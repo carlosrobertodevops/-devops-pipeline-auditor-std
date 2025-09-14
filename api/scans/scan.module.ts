1a281ed29df0849d6a6c45d4a6c875185230a217
Carlos Roberto dos Santos Silva <carlos.roberto.devops@gmail.com>
2025-09-14 20:57:27 -0300

    Delete api/scans/scan.module.ts

--- api/scans/scan.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { PrismaService } from '../common/prisma.service'
-import { ScansService } from './scans.service'
-import { ScansController } from './scans.controller'
-import { FindingsService } from '../findings/findings.service'
-
-@Module({ controllers:[ScansController], providers:[PrismaService, ScansService, FindingsService] })
-export class ScansModule {}
